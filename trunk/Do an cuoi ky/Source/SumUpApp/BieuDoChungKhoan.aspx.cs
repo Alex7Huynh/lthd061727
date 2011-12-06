c43db801c3a5aa1eb464f110a7a7a29325110330
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@87a2a727-418a-6b9d-503a-9ee094c269d6>
2011-12-06 17:16:49 +0000

    Khung chương trình đồ án cuối kì ( rút trích dữ liệu chứng khoán ).

---
+++ trunk/Do an cuoi ky/Source/SumUpApp/BieuDoChungKhoan.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SumUpApp
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Người dùng chọn 1 chứng khoán bất kì
+            // Hệ thống load lên thông tin của chứng khoán đó và hiển thị thông qua canvas của html5
+        }
+    }
+}
