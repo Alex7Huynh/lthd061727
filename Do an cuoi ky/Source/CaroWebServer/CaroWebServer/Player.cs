7a78ba6656d340da856180c7bbc3c8937b7107be
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@87a2a727-418a-6b9d-503a-9ee094c269d6>
2011-12-22 10:36:33 +0000

    Add server.

---
+++ Do an cuoi ky/Source/CaroWebServer/CaroWebServer/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaroWebServer
+{
+    public class Player
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public int Id
+        {
+            get;
+            set;
+        }
+    }
+}
