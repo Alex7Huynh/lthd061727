982589e2e05a83f2d0d5f9dbf749eca10e17fd62
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@87a2a727-418a-6b9d-503a-9ee094c269d6>
2011-12-22 08:51:26 +0000

    Fix related position pointer.

--- trunk/Do an cuoi ky/Source/GM/Scripts/caro.js
+++ trunk/Do an cuoi ky/Source/GM/Scripts/caro.js
@@ -358,8 +358,35 @@ function paintO(x, y) {
 }
 
 function clickHandler(e) {
-    var y = Math.floor((e.clientY-8) / (height / nmbRows));    
-    var x =  Math.floor((e.clientX-8) / (width/ nmbColumns)); 
+    // jQuery would normalize the event
+    position = getPosition(e);
+    //now you can use the x and y positions
+    //alert("X: " + position.x + " Y: " + position.y);
+
+    var y = Math.floor((position.y) / (height / nmbRows));
+    var x = Math.floor((position.x) / (width / nmbColumns)); 
 
     clk(x,y);
 }
+
+function getPosition(e) {
+
+    //this section is from http://www.quirksmode.org/js/events_properties.html
+    var targ;
+    if (!e)
+        e = window.event;
+    if (e.target)
+        targ = e.target;
+    else if (e.srcElement)
+        targ = e.srcElement;
+    if (targ.nodeType == 3) // defeat Safari bug
+        targ = targ.parentNode;
+
+    // jQuery normalizes the pageX and pageY
+    // pageX,Y are the mouse positions relative to the document
+    // offset() returns the position of the element relative to the document
+    var x = e.pageX - $(targ).offset().left;
+    var y = e.pageY - $(targ).offset().top;
+
+    return { "x": x, "y": y };
+};
--- trunk/Do an cuoi ky/Source/GM/Util/PlayCaro.aspx
+++ trunk/Do an cuoi ky/Source/GM/Util/PlayCaro.aspx
@@ -2,6 +2,7 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
     <title>Play Caro With Your Friend</title>
     <script src="../Scripts/caro.js" type="text/javascript"></script>
+    <script type="text/javascript" src="../Scripts/jquery-1.4.1.js"></script>  
     <link type="text/css" href="../Styles/Caro.css" rel="stylesheet"/>
     <script type="text/javascript">
         function loadForm() {
@@ -10,6 +11,6 @@
     </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    <canvas onclick="javascript: clickHandler(event);" id="board" height="600" width="600"/>
+    <canvas onclick="javascript: clickHandler(event);" id="board" height="600" width="600" position="relative"/>
     <input type="button" onclick="javascript: resetGame();" value="reset" name="Reset"/>
 </asp:Content>
