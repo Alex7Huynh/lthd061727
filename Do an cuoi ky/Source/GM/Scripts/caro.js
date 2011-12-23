6653352476611c0849cadefe23bd0d26d6ccab6c
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@87a2a727-418a-6b9d-503a-9ee094c269d6>
2011-12-23 08:49:40 +0000

    Bổ sung ajax

--- Do an cuoi ky/Source/GM/Scripts/caro.js
+++ Do an cuoi ky/Source/GM/Scripts/caro.js
@@ -21,45 +21,43 @@ for (i = 0; i < boardSize; i++) {
 
 function paintBoard() {
     var board = document.getElementById('board');
-  
+
     width = board.width;
     height = board.height;
     context = board.getContext('2d');
 
     context.beginPath();
-    context.strokeStyle = '#000'; 
-    context.lineWidth   = 2;
+    context.strokeStyle = '#000';
+    context.lineWidth = 2;
 
-    var i=0; 
-    for (i=0;i<=nmbRows+1;i++)
-    {
-       context.moveTo(0, (height / nmbRows) * i);
-       context.lineTo(width, (height / nmbRows) * i);
+    var i = 0;
+    for (i = 0; i <= nmbRows + 1; i++) {
+        context.moveTo(0, (height / nmbRows) * i);
+        context.lineTo(width, (height / nmbRows) * i);
     }
-    var j =0;
-    for (j=0;j<=nmbColumns+1;j++)
-    {
+    var j = 0;
+    for (j = 0; j <= nmbColumns + 1; j++) {
         context.moveTo((width / nmbColumns) * j, 0);
         context.lineTo((width / nmbColumns) * j, height);
     }
 
     context.stroke();
-    context.closePath(); 
+    context.closePath();
 }
 
 function resetGame() {
-    oppTurn=true;
-    gameOver=false;
+    oppTurn = true;
+    gameOver = false;
 
-    for (i=0;i<boardSize;i++) {
-        for (j=0;j<boardSize;j++) {
-            f[i][j]=0;
+    for (i = 0; i < boardSize; i++) {
+        for (j = 0; j < boardSize; j++) {
+            f[i][j] = 0;
         }
     }
-    
+
     var board = document.getElementById('board');
     context = board.getContext('2d');
-    context.clearRect(0,0,width,height);
+    context.clearRect(0, 0, width, height);
 
     paintBoard();
 
@@ -67,61 +65,61 @@ function resetGame() {
 }
 
 
-function drawSquare(x,y,who) {
-    if (who==1) {//user 
-        paintO(x,y);
+function drawSquare(x, y, who) {
+    if (who == 1) {//user 
+        paintO(x, y);
     }
-    else if (who==-1){//machine
-        paintX(x,y);
+    else if (who == -1) {//machine
+        paintX(x, y);
     }
 }
 
 function paintX(x, y) {
 
-   context.beginPath();
+    context.beginPath();
 
-   context.strokeStyle = '#ff0000'; 
-   context.lineWidth   = 2;
+    context.strokeStyle = '#ff0000';
+    context.lineWidth = 2;
 
-   var offsetX = (width / nmbColumns) * 0.1;
-   var offsetY = (height / nmbRows) * 0.1;
+    var offsetX = (width / nmbColumns) * 0.1;
+    var offsetY = (height / nmbRows) * 0.1;
 
-   var beginX = x * (width / nmbColumns) + offsetX;
-   var beginY = y * (height / nmbRows) + offsetY;
+    var beginX = x * (width / nmbColumns) + offsetX;
+    var beginY = y * (height / nmbRows) + offsetY;
 
-   var endX = (x + 1) * (width / nmbColumns) - offsetX * 2;
-   var endY = (y + 1) * (height / nmbRows) - offsetY * 2;
+    var endX = (x + 1) * (width / nmbColumns) - offsetX * 2;
+    var endY = (y + 1) * (height / nmbRows) - offsetY * 2;
 
-   context.moveTo(beginX, beginY);
-   context.lineTo(endX, endY); 
+    context.moveTo(beginX, beginY);
+    context.lineTo(endX, endY);
 
-   context.moveTo(beginX, endY);
-   context.lineTo(endX, beginY); 	
+    context.moveTo(beginX, endY);
+    context.lineTo(endX, beginY);
 
-   context.stroke();
-   context.closePath(); 
+    context.stroke();
+    context.closePath();
 }
 
 function paintO(x, y) {
-	
-   context.beginPath();
 
-   context.strokeStyle = '#0000ff'; 
-   context.lineWidth   = 2;
+    context.beginPath();
 
-   var offsetX = (width / nmbColumns) * 0.1;
-   var offsetY = (height / nmbRows) * 0.1;
+    context.strokeStyle = '#0000ff';
+    context.lineWidth = 2;
 
-   var beginX = x * (width / nmbColumns) + offsetX;
-   var beginY = y * (height / nmbRows) + offsetY;
+    var offsetX = (width / nmbColumns) * 0.1;
+    var offsetY = (height / nmbRows) * 0.1;
 
-   var endX = (x + 1) * (width / nmbColumns) - offsetX * 2;
-   var endY = (y + 1) * (height / nmbRows) - offsetY * 2;
+    var beginX = x * (width / nmbColumns) + offsetX;
+    var beginY = y * (height / nmbRows) + offsetY;
 
-   context.arc(beginX + ((endX - beginX) / 2), beginY + ((endY - beginY) / 2), (endX - beginX) / 2 , 0, Math.PI * 2, true);
+    var endX = (x + 1) * (width / nmbColumns) - offsetX * 2;
+    var endY = (y + 1) * (height / nmbRows) - offsetY * 2;
 
-   context.stroke();
-   context.closePath();
+    context.arc(beginX + ((endX - beginX) / 2), beginY + ((endY - beginY) / 2), (endX - beginX) / 2, 0, Math.PI * 2, true);
+
+    context.stroke();
+    context.closePath();
 }
 
 function clickHandler(e) {
@@ -131,9 +129,9 @@ function clickHandler(e) {
     //alert("X: " + position.x + " Y: " + position.y);
 
     var y = Math.floor((position.y) / (height / nmbRows));
-    var x = Math.floor((position.x) / (width / nmbColumns)); 
+    var x = Math.floor((position.x) / (width / nmbColumns));
 
-    clk(x,y);
+    clk(x, y);
 }
 
 function getPosition(e) {
@@ -168,7 +166,7 @@ function clk(iMove, jMove) {
         oppTurn = true;
         UserMove(iMove, jMove);
         WaitingForOpponent();
-    
+
         CheckGameOver();
     }
 }
--- Do an cuoi ky/Source/GM/Util/PlayCaro.aspx.designer.cs
+++ Do an cuoi ky/Source/GM/Util/PlayCaro.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace CaroSocialNetwork {
     public partial class PlayCaro {
         
         /// <summary>
+        /// AjaxScriptManager control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager AjaxScriptManager;
+        
+        /// <summary>
         /// btnPlayWithMachine control.
         /// </summary>
         /// <remarks>
