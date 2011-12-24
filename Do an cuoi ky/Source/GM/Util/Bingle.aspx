2b67799973d72037dc003049af2c2f25e2ded770
7hanatos13@gmail.com <7hanatos13@gmail.com@87a2a727-418a-6b9d-503a-9ee094c269d6>
2011-12-24 07:09:08 +0000

    Bổ sung style, script, trang GMap, Bingle, BingTranslate.

---
+++ Do an cuoi ky/Source/GM/Util/Bingle.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Map Viewer" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="Bingle.aspx.cs" Inherits="CaroSocialNetwork.Bingle" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <link href="../Styles/MyStyle.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="../Scripts/gears_init.js"></script>
+    <script type="text/javascript" src="../Scripts/MyGoogleMap.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&language=vi"></script>    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    
+</asp:Content>
---
+++ Do an cuoi ky/Source/GM/Util/GMap.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+using System.Xml.Linq;
+using System.Web.Security;
+using System.Web.UI.HtmlControls;
+using System.Configuration;
+
+namespace CaroSocialNetwork
+{
+    public partial class GMap : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static void LoadTreeView(ref TreeView treeView)
+        { 
+        
+        }
+
+        protected void TreeView1_TreeNodeCheckChanged(object sender, TreeNodeEventArgs e)
+        { 
+        
+        }
+
+        protected void btnDangXuat_Click(object sender, EventArgs e)
+        { 
+        
+        }
+
+        [System.Web.Services.WebMethod]
+        public static int ThemDiaDiem(string tenDiaDiem, float viDo, float kinhDo, string ghiChu, int maDanhMuc, string tenDanhMuc)
+        {
+            return 1;
+        }
+
+        [System.Web.Services.WebMethod]
+        public static bool CapNhatDiaDiem(int maDiaDiem, string tenDiaDiem, float viDo, float kinhDo, string ghiChu)
+        {
+            return true;
+        }
+
+        [System.Web.Services.WebMethod]
+        public static bool XoaDiaDiem(int maDiaDiem)
+        {
+            return true;
+        }
+
+        public void LoadMyTreeView()
+        { 
+        
+        }
+
+        [System.Web.Services.WebMethod]
+        public static string TimDiaDiemGanNhat(float viDoHienTai, float kinhDoHienTai, string danhMucTimKiem)
+        {
+            return String.Empty;
+        }
+
+        //public static double FindDistance(DiaDiemDTO p1, DiaDiemDTO p2)
+        //{
+        //    double R = 6371; // earth's mean radius in km
+        //    double dLat = rad(p2.ViDo - p1.ViDo);
+        //    double dLong = rad(p2.KinhDo - p1.KinhDo);
+
+        //    double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+        //            Math.Cos(rad(p1.ViDo)) * Math.Cos(rad(p2.ViDo)) * Math.Sin(dLong / 2) * Math.Sin(dLong / 2);
+        //    double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        //    double d = R * c;
+
+        //    return d;
+        //}
+        
+        public static double rad(double x)
+        {
+            return x * Math.PI / 180;
+        }
+    }
+}
