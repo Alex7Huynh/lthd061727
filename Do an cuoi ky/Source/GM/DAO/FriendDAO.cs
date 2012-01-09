163b38495185e1037e114f2355ba861891fd0c8e
tienvx2008gs@gmail.com <tienvx2008gs@gmail.com@87a2a727-418a-6b9d-503a-9ee094c269d6>
2012-01-09 18:58:35 +0000

    Kết bạn thành công.

---
+++ Do an cuoi ky/Source/GM/DAO/FriendDAO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaroSocialNetwork.DAO
+{
+    public class FriendDAO
+    {
+        internal static void MakeFriend(Guid myid, Guid iduser)
+        {
+            try
+            {
+                MashDataClassesDataContext db = new MashDataClassesDataContext();
+                Friend friend = new Friend();
+                friend.UserId = myid;
+                friend.FriendId = iduser;
+                db.Friends.InsertOnSubmit(friend);
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
